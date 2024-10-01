fc0a9b9081631aa205845fc654bf6dd21c214a85
Nonymost <maharjananujraja@gmail.com>
2024-10-01 16:34:33 +0545

    static page

--- index.cjs
+++ index.cjs
@@ -6,6 +6,7 @@ const morgan = require("morgan")
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.static("dist"))
 
 morgan.token("data", (req, res) => {
     return req.method === "POST" ? JSON.stringify(req.body) : "";
